3aad2d14713b0286b6a2cbed56900a5fd615a1c3
Vatsal Goyal <vatsalgoyal24@gmail.com>
2025-12-14 12:50:55 +0530

    done

--- AgentFlowX/backend/queue.js
+++
@@ -1,8 +0,0 @@
-const { Queue } = require('bullmq');
-const Redis = require('ioredis');
-
-const connection = new Redis(process.env.REDIS_URL);
-
-const agentQueue = new Queue('agent-jobs', { connection });
-
-module.exports = { agentQueue };
