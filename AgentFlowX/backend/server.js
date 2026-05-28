3cde6001ccb33ed27ebb3a3c0cc5fa559f061255
Vatsal Goyal <vatsalgoyal24@gmail.com>
2026-05-28 16:27:28 +0530

    fix express options crash

--- AgentFlowX/backend/server.js
+++ AgentFlowX/backend/server.js
@@ -20,12 +20,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
-
-app.options("*", cors());
-
 app.use(bodyParser.json());
-
-
 /* =========================
    ROUTES
 ========================= */
